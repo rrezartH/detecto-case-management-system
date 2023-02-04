b5d1f615a2f8670f481d35f03c613b11291d310c
zekagranit1 <64788935+zekagranit1@users.noreply.github.com>
2023-02-04 20:33:13 +0100

    i made some filters
    i added some filters for tasks

--- client/src/pages/Task/TaskList/TaskList.jsx
+++ client/src/pages/Task/TaskList/TaskList.jsx
@@ -3,13 +3,28 @@ import "./task-list.scss";
 import TaskCard from "../../../components/TaskCard/TaskCard"
 import agent from "../../../api/agents";
 import CreateTask from "../CreateTask/CreateTask";
+import moment from 'moment';
+
 
 const TaskList = () => {
   const [taskat, setTaskat] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
+  const [filters, setFilters] = useState({
+    statusi: "all",
+    dueDate: "",
+  });
 
   const handleOpen = () => {
     setIsOpen((prev) => !prev); 
+  };
+
+  const handleFilterChange = (e) => {
+    e.preventDefault();
+    setFilters({
+      ...filters,
+      [e.target.name]: e.target.value
+    });
+    console.log(filters.statusi);
   };
 
   useEffect(() => {
@@ -18,16 +33,44 @@ const TaskList = () => {
     });
   }, []);
 
+  let filteredTasks = taskat
+  
+  if (filters.statusi !== "all") {
+    filteredTasks = filteredTasks.filter(
+      task => String(task.statusi) === filters.statusi
+      );
+    }
+    
+    if (filters.dueDate !== "") {
+      filteredTasks = filteredTasks.filter(
+      task => moment(task.dueDate).format('YYYY-MM-DD') === filters.dueDate
+    );
+  }
+
   return (
     <>
       <h1>Taskat</h1>
-      <button className="card-layout__add" onClick={handleOpen}>
-          + Shto task
-        </button>
+        <button className="card-layout__add" onClick={handleOpen}>
+            + Shto task
+          </button>
+      <div className="filters">
+      <label htmlFor="statusi-filter">Filter by due date:</label>
+        <select
+          id="statusi-filter"
+          name="statusi"
+          value={filters.statusi}
+          onChange={handleFilterChange}
+        >
+          <option value="all">All</option>
+          <option value="true">Done</option>
+          <option value="false">Not Done</option>
+        </select>
+        <label htmlFor="due-date-filter">Filter by due date:</label>
+        <input type="date" id="due-date-filter" value={filters.dueDate} onChange={e => setFilters({...filters, dueDate: e.target.value})} />
+      </div>
       <div className="card-layout column">
-        
         {React.Children.toArray(
-          taskat.map((taskat) => (
+          filteredTasks.map((taskat) => (
             <TaskCard
               taskID={taskat.id}
               title={taskat.title}
