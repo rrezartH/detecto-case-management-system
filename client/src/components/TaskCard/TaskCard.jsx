6239dfdf4879c733dac75b6ad91c750ffa885c21
zekagranit1 <64788935+zekagranit1@users.noreply.github.com>
2023-02-04 19:04:56 +0100

    changed small detail
    small naming detail so the scope of my scss file is smaller

--- client/src/components/TaskCard/TaskCard.jsx
+++ client/src/components/TaskCard/TaskCard.jsx
@@ -15,29 +15,29 @@ const TaskCard = ({ taskID,title,details, dateCreated, dueDate, statusi}) => {
   dateCreated = moment(dateCreated).format('YYYY-MM-DD');
 
   return (
-    <div className="card row">
+    <div className="cardT row">
 
       <TaskPopup isOpen={showPopup} setIsOpen={setShowPopup} task={{ title, details, dueDate, taskID, statusi }} />
       
 
       <div className="main-text column">
-        <p className="card-id">ID: {taskID}</p>
+        <p className="cardT-id">ID: {taskID}</p>
         <div className="row">
-        <h1 className="card-title">{title}</h1>
+        <h1 className="cardT-title">{title}</h1>
         
-        {statusi ? <h1 className="card-status done">Done</h1> 
-        : <h1 className="card-status">Doing</h1> }
+        {statusi ? <h1 className="cardT-status done">Done</h1> 
+        : <h1 className="cardT-status">Doing</h1> }
           
         </div>
-        <p className="card-details">{details}</p>
-        <p className="card-date">Created: {dateCreated}</p>
+        <p className="cardT-details">{details}</p>
+        <p className="cardT-date">Created: {dateCreated}</p>
       </div>
 
-      <div className="card-extra column">
+      <div className="cardT-extra column">
         <div className="center">
-        <h3 className="card-due">DUE: {moment(dueDate).format('DD/MM/YYYY')}</h3>
+        <h3 className="cardT-due">DUE: {moment(dueDate).format('DD/MM/YYYY')}</h3>
         </div>
-        <div className="card-button">
+        <div className="cardT-button">
         <button className="button" onClick={handleShowPopup} >SHIKO</button>
         </div>
       </div>
